1c134f425f6abe36108a6706be1fe481dddaf55e
Henrik Paul <henrik.paul@gmail.com>
2010-03-31 15:32:05 +0300

    Laxed the memory test a bit.

--- test/com/github/wolfie/blackboard/MemLeakTest.java
+++ test/com/github/wolfie/blackboard/MemLeakTest.java
@@ -8,27 +8,29 @@ public class MemLeakTest {
     public void method(final MemTestEvent event) {
     }
   }
-  
+
   static class MemTestEvent extends Event {
   }
-  
+
   private static final Blackboard BLACKBOARD = new Blackboard();
-  private static final int TEST_MAX = 10000000;
+  private static final int TEST_MAX = 1000000;
   private static final Runtime RUNTIME = Runtime.getRuntime();
-  
+
   public static void main(final String args[]) {
     BLACKBOARD.register(MemTestListener.class, MemTestEvent.class);
-    
+
     for (int i = 0; i < TEST_MAX; i++) {
       BLACKBOARD.addListener(new MemTestListener());
       if (i % 10000 == 0) {
         final long freeMemory = RUNTIME.freeMemory();
         final long totalMemory = RUNTIME.totalMemory();
-        System.out.println(i + ": " + ((totalMemory - freeMemory) / 1024)
-            + "KB");
+
+        final String usedMemoryKb = String
+            .valueOf((totalMemory - freeMemory) / 1024);
+        System.out.println("iteration " + i + ": " + usedMemoryKb + "KB used.");
       }
     }
-    
+
     System.out.println(TEST_MAX + " iterations passed!");
   }
 }
